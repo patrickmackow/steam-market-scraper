31fd6123294210a34f5bf6c7d2df0c07f66055f1
Patrick Mackow <patrickmackow@gmail.com>
2013-12-03 19:57:22 -0500

    Removed test functions.

--- csgo-market-full.js
+++ csgo-market-full.js
@@ -1,37 +1,19 @@
 var url = "http://steamcommunity.com/market/search?q=appid%3A730#p102"
 
+// Casper settings
 var casper = require("casper").create({
     pageSettings: {
-        //        loadImages: false,
+        loadImages: false, // Load pages quicker
         userAgent: "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0"
     }
 });
 
 casper.start(url);
 
-/*casper.then(function() {
-  var ready = this.evaluate(function() {
-  return document.readyState == "complete";
-  });
-  this.echo(ready);
-  });*/
-
 casper.then(function() {
     this.waitUntilVisible("a.market_listing_row_link", function() {
-        this.capture('test.png');
-        this.echo(this.getPageContent());
-        this.echo(this.getHTML("div#searchResultsTable"));
+        this.echo(this.getHTML("a.market_listing_row_link"));
     });
 });
-
-
-
-/*casper.then(function() {
-  var js = this.evaluate(function() {
-  return document.documentElement.outerHTML;
-  });
-
-  this.echo(js);
-  });*/
 
 casper.run();
