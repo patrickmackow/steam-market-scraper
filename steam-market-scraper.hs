7b14854ec8f1282dc648c7f3461e724382c22936
Patrick Mackow <patrickmackow@gmail.com>
2013-12-28 13:52:15 -0500

    Added dependencies and testing how processes work in Haskell.

--- steam-market-scraper.hs
+++ steam-market-scraper.hs
@@ -1,4 +1,14 @@
+import Text.HTML.TagSoup
+import System.IO
+import System.Process
 import Paths_steam_market_scraper
 
 main :: IO()
-main = getDataFileName "market-total.js" >>= print
+main = do
+    marketTotal <- getDataFileName "market-total.js" 
+    print $ (marketTotal ++ " 'http://steamcommunity.com/market/search?q=appid%3A730'")
+    handle <- runCommand $ ("casperjs " ++ marketTotal ++ " 'http://steamcommunity.com/market/search?q=appid%3A730'")
+    exitCode <- waitForProcess handle
+    print exitCode
+    handle2 <- runCommand $ ("casperjs market-total.js 'http://steamcommunity.com/market/search?q=appid%3A730'")
+    waitForProcess handle2 >>= print
