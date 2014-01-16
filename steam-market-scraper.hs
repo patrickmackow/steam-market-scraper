09f48d3389a9c1a04c1814bd25e2217889ca1bd9
Patrick <patrick@tsar-bomba>
2014-01-16 11:43:05 -0500

    Commented out test functions and added function types.

--- steam-market-scraper.hs
+++ steam-market-scraper.hs
@@ -3,9 +3,18 @@ import System.IO
 import System.Process
 import Paths_steam_market_scraper
 
+data MarketItem = undefined
+data ItemListing = undefined
+
 main :: IO()
 main = do
-    handle <- runCommand $ ("casperjs market-total.js 'http://steamcommunity.com/market/search?q=appid%3A730'")
-    waitForProcess handle >>= print
-    handle2 <- runCommand $ ("casperjs market-page.js 'http://steamcommunity.com/market/search?q=appid%3A730#p50' csgo-pages/")
-    waitForProcess handle2 >>= print
+    --handle <- runCommand $ ("casperjs market-total.js 'http://steamcommunity.com/market/search?q=appid%3A730'")
+    --waitForProcess handle >>= print
+    --handle2 <- runCommand $ ("casperjs market-page.js 'http://steamcommunity.com/market/search?q=appid%3A730#p50' csgo-pages/")
+    --waitForProcess handle2 >>= print
+    
+scrapeMarketPage :: Text -> [MarketItem]
+undefined
+
+scarapeItemPage :: Text -> [ItemListing]
+undefined
