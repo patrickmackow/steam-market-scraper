d3fcf66990df2dbf46a08740c826ef720602a309
Patrick <patrick@tsar-bomba>
2014-01-21 11:53:37 -0500

    Created functions for each sections to be scraped. Price still needs to be fixed.

--- steam-market-scraper.hs
+++ steam-market-scraper.hs
@@ -1,23 +1,71 @@
 import Text.HTML.TagSoup
 import System.IO
 import System.Process
+
 import Paths_steam_market_scraper
 
-data MarketItem = undefined
-data ItemListing = undefined
+data MarketItem = MarketItem { link :: String
+                             , image :: String
+                             , quantity :: String
+                             , price :: String
+                             , name :: String
+                             , game :: String
+                             } deriving (Show)
 
 main :: IO()
 main = do
-    --handle <- runCommand $ ("casperjs market-total.js\
-    --\'http://steamcommunity.com/market/search?q=appid%3A730'")
-    --waitForProcess handle >>= print
-    --handle2 <- runCommand $ ("casperjs market-page.js\
-    --\'http://steamcommunity.com/market/search?q=appid%3A730#p50'\
-    --\csgo-pages/")
-    --waitForProcess handle2 >>= print
-    
-scrapeMarketPage :: Text -> [MarketItem]
-undefined
+    -- handle <- runCommand $ ("casperjs market-total.js\
+    -- \'http://steamcommunity.com/market/search?q=appid%3A730'")
+    -- waitForProcess handle >>= print
+    -- handle2 <- runCommand $ ("casperjs market-page.js\
+    -- \'http://steamcommunity.com/market/search?q=appid%3A730#p50'\
+    -- \csgo-pages/")
+    -- waitForProcess handle2 >>= print
+    test <- readFile "csgo-pages/50"
+    let items = scrapeMarketPage test
+    print items
+    print $ length items
 
-scarapeItemPage :: Text -> [ItemListing]
-undefined
+scrapeMarketPage :: String -> [MarketItem]
+scrapeMarketPage page = fmap scrapeMarketItem items
+    where items = sections (~== "<a class=market_listing_row_link>")
+                  $ parseTags page
+
+scrapeMarketItem :: [Tag String] -> MarketItem
+scrapeMarketItem item = MarketItem link image quantity price name game
+    where link = scrapeLink item
+          image = scrapeImage item
+          quantity = scrapeQuantity item
+          price = scrapePrice item
+          name = scrapeName item
+          game = scrapeGame item
+
+scrapeLink :: [Tag String] -> String
+scrapeLink item = getAttrib "href" item
+
+scrapeImage :: [Tag String] -> String
+scrapeImage item = getAttrib "src" image
+    where image = head . sections (~== "<img>") $ item
+
+scrapeQuantity :: [Tag String] -> String
+scrapeQuantity item = getTagText quantity
+    where quantity = head . sections (~== "<span class=market_listing_num_listings_qty>") $ item
+
+scrapePrice :: [Tag String] -> String
+scrapePrice item = innerText price
+    where price = takeWhile (~/= "<div class=market_list_name_item_block>") $ dropWhile (~/= "<br>") item
+
+scrapeName :: [Tag String] -> String
+scrapeName item = getTagText name
+    where name = head . sections (~== "<span class=market_listing_item_name>") $ item
+
+scrapeGame :: [Tag String] -> String
+scrapeGame item = getTagText game
+    where game = head . sections (~== "<span class=market_listing_game_name>") $ item
+
+-- Util functions
+getAttrib :: String -> [Tag String] -> String
+getAttrib atr tag = fromAttrib atr . head . filter isTagOpen $ tag
+
+getTagText :: [Tag String] -> String
+getTagText = fromTagText . head . filter isTagText
