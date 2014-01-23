cde8ba619a645243f891f01063af8ad7c5790421
Patrick <patrick@tsar-bomba>
2014-01-23 17:12:55 -0500

    Added trigger to insert market_history automatically.

--- steam_market.sql
+++ steam_market.sql
@@ -1,6 +1,10 @@
 ﻿-- Database: steam_market
 
 -- DROP DATABASE steam_market;
+DROP TRIGGER IF EXISTS insert_market ON market;
+DROP FUNCTION IF EXISTS insert_market_history();
+DROP TABLE IF EXISTS market_history;
+DROP TABLE IF EXISTS market;
 
 CREATE TABLE market (
 	id serial PRIMARY KEY,
@@ -15,16 +19,22 @@ CREATE TABLE market (
 
 CREATE TABLE market_history (
 	id serial PRIMARY KEY,
-	url text REFERENCES market,
+	url text REFERENCES market (url),
 	quantity int NOT NULL,
 	price numeric(7,2) NOT NULL,
 	market_timestamp timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TRIGGER insert_market
-	AFTER INSERT, UPDATE ON market
-	FOR EACH ROW
-	EXECUTE PROCEDURE insert_market_history(url, quantity, price);
+CREATE FUNCTION insert_market_history() RETURNS trigger AS $$
+	BEGIN
+		INSERT INTO market_history (url, quantity, price) 
+			VALUES (NEW.url, NEW.quantity, NEW.price);
+		RETURN NULL;
+	END;
+$$ LANGUAGE plpgsql;
 
-CREATE FUNCTION insert_market_history
+CREATE TRIGGER insert_market
+	AFTER INSERT OR UPDATE ON market
+	FOR EACH ROW
+	EXECUTE PROCEDURE insert_market_history();
 	
