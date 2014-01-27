d12feafa8f6b0b69925b2ac5d1c68dc7461a3b90
Patrick <patrick@tsar-bomba>
2014-01-27 11:09:49 -0500

    currency-rates now writes to json files to be used by listing scraper.

--- currency-rates.hs
+++ currency-rates.hs
@@ -1,6 +1,10 @@
 import Text.HTML.TagSoup
 import Network.HTTP
 import Network.Stream
+import Data.Aeson
+import qualified Data.Text as T
+import System.IO
+import qualified Data.ByteString.Lazy.Char8 as B
 
 ratesURLs = [ 
     "http://www.xe.com/currencyconverter/convert/?Amount=1&From=RUB&To=USD",
@@ -9,24 +13,35 @@ ratesURLs = [
     "http://www.xe.com/currencyconverter/convert/?Amount=1&From=EUR&To=USD"
     ]
 
+data CurrencyRate = CurrencyRate
+    { currency :: String
+    , rate :: Double
+    } deriving (Show)
+
+instance ToJSON CurrencyRate where
+    toJSON (CurrencyRate currency rate) = 
+        object [T.pack "currency" .= currency, T.pack "rate" .= rate]
+
 main :: IO ()
 main = do
     srcs <- mapM openURL ratesURLs
     let rates = fmap scrapeCurrencyPage srcs
-    print rates
-    -- let tags = map parseTags srcmap (sections (~== "<tr class=uccRes>")) tags
+    let json = fmap B.unpack $ fmap encode rates
+    writeFile "rates.json" $ unlines json
 
 openURL :: String -> IO String
 openURL x = getResponseBody =<< simpleHTTP (getRequest x)
 
-scrapeCurrencyPage :: String -> (String, String)
+scrapeCurrencyPage :: String -> CurrencyRate
 scrapeCurrencyPage page = scrapeRate tags
     where tags = head . sections (~== "<tr class=uccRes>") $ parseTags page
 
-scrapeRate :: [Tag String] -> (String, String)
-scrapeRate tags = (currency, rate)
-    where currency = getTagText . head $ sections (~== "<span class=uccResCde>") tags
-          rate = getTagText . head $ sections (~== "<td class=rightCol>") tags
+scrapeRate :: [Tag String] -> CurrencyRate
+scrapeRate tags = CurrencyRate currency $ read rate
+    where currency = getTagText . head $ 
+            sections (~== "<span class=uccResCde>") tags
+          rate = takeWhile (/= '\160') . getTagText . head $ 
+            sections (~== "<td class=rightCol>") tags
 
 -- Util functions
 getAttrib :: String -> [Tag String] -> String
