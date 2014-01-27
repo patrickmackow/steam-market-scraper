23088eee5bda36a5590a69de83cbb97bccecd6e4
Patrick <patrick@tsar-bomba>
2014-01-27 01:23:44 -0500

    Commiting just to work on it on laptop.

--- steam-listing-scraper.hs
+++ steam-listing-scraper.hs
@@ -72,28 +72,28 @@ main = do
     -- print $ length items
 
 -- Download all Steam Market pages
-scrapeMarket :: Int -> IO ()
-scrapeMarket 1 = do
+scrapeListings :: Int -> IO ()
+scrapeListings 1 = do
     handle <- runCommand $ ("casperjs market-page.js\
     \ 'http://steamcommunity.com/market/search?q=appid%3A730#p"
         ++ (show 1) ++ "' csgo-pages/")
     waitForProcess handle
     return ()
-scrapeMarket x = do
+scrapeListings x = do
     handle <- runCommand $ ("casperjs market-page.js\
     \ 'http://steamcommunity.com/market/search?q=appid%3A730#p"
         ++ (show x) ++ "' csgo-pages/")
     waitForProcess handle
     scrapeMarket (x - 1)
 
-readMarketPage :: FilePath -> IO [MarketItem]
-readMarketPage path = do
+readListingsPage :: FilePath -> IO [ItemListing]
+readListingsPage path = do
     file <- readFile path
-    removeFile path
-    return $ scrapeMarketPage file
+    -- removeFile path
+    return $ scrapeListingsPage file
 
-storeItems :: Connection -> [MarketItem] -> IO ()
-storeItems conn items = do
+storeListings :: Connection -> [MarketItem] -> IO ()
+storeListings conn items = do
     let select = Query $ B.pack "SELECT url FROM market"
     existing <- query_ conn select
     let existingStrings = fmap (\(Only url) -> B.unpack url) existing
@@ -102,31 +102,20 @@ storeItems conn items = do
     forM_ items $ \item -> checkItem conn existingStrings item
     return ()
 
-checkItem :: Connection -> [String] -> MarketItem -> IO Int64
-checkItem conn existing item
-    | length existing == 0 = insertItem conn item
-    | (url item) `elem` existing = updateItem conn [quantity item, price item, url item]
-    | otherwise = insertItem conn item
-
-insertItem :: Connection -> MarketItem -> IO Int64
-insertItem conn item = execute conn insert item
+insertListings :: Connection -> MarketItem -> IO Int64
+insertListings conn item = execute conn insert item
     where insert = Query $ B.pack "INSERT INTO market (url, image, \
              \quantity, price, item_name, item_name_colour, game) \
              \VALUES (?,?,?,?,?,?,?)"
 
-updateItem :: Connection -> [String] -> IO Int64
-updateItem conn item = execute conn update item
-    where update = Query $ B.pack "UPDATE market SET quantity = ?, \
-             \price = ? WHERE url = ?"
-
 -- Scrape info from market page
-scrapeMarketPage :: String -> [MarketItem]
-scrapeMarketPage page = fmap scrapeMarketItem items
-    where items = sections (~== "<a class=market_listing_row_link>")
+scrapeListingsPage :: String -> [ItemListing]
+scrapeListingsPage page = fmap scrapeItemListing listings
+    where listings = sections (~== "<div class=market_listing_row>")
                   $ parseTags page
 
-scrapeMarketItem :: [Tag String] -> MarketItem
-scrapeMarketItem item = 
+scrapeItemListing :: [Tag String] -> ItemListing
+scrapeItemListing listing = 
     MarketItem url image quantity price name nameColour game
     where url = scrapeUrl item
           image = scrapeImage item
@@ -158,24 +147,6 @@ scrapePrice item = g . fromTagText . head $ priceText
           -- Get price only
           g :: String -> String
           g x = drop 1 . takeWhile (/= ' ') $ dropWhile (/= '$') x
-
-scrapeName :: [Tag String] -> String
-scrapeName item = getTagText name
-    where name = head . sections (~== "<span\ 
-        \ class=market_listing_item_name>") $ item
-
-scrapeNameColour :: [Tag String] -> String
-scrapeNameColour item = f $ getAttrib "style" colour
-    where colour = head . sections (~== "<span\ 
-        \ class=market_listing_item_name>") $ item
-          -- Get CSS colour
-          f :: String -> String
-          f x = takeWhile (/= ';') $ dropWhile (/= '#') x
-
-scrapeGame :: [Tag String] -> String
-scrapeGame item = getTagText game
-    where game = head . sections (~== "<span\ 
-        \ class=market_listing_game_name>") $ item
 
 -- Util functions
 getAttrib :: String -> [Tag String] -> String
