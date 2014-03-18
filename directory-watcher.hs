3872d70f4b9662d2a0eadbc41bd4804747c5bae5
Patrick <patrick@tsar-bomba>
2014-03-18 01:31:10 -0400

    Directory watcher uses state monad to keep track of directory contents. TODO: Add processing for files.

--- directory-watcher.hs
+++ directory-watcher.hs
@@ -1,13 +1,20 @@
 import Control.Concurrent
 import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State
 import Data.Either.Utils
+import Data.List
 import Data.List.Split
+import System.Directory
 import qualified Data.ConfigFile as C
+import qualified Data.Set as Set
 
 data Directory = Directory
     { path :: String
     , contents :: [String]
     } deriving (Show, Eq)
+
+type DirectoryS a = StateT [Directory] IO a
 
 main :: IO ()
 main = do
@@ -16,6 +23,44 @@ main = do
     let directories = splitOn "," $ forceEither $ C.get
             configValues "DEFAULT" "directories"
     print directories
+    let directoryList = map (\x -> Directory x []) directories
+    print directoryList
 
-    forever $ do
-        threadDelay 1000000
+    runStateT loop directoryList
+    return ()
+
+loop :: DirectoryS ()
+loop = do
+    directories <- get
+    directories' <- liftIO $ execStateT checkDirectories $ directories
+
+    let newFiles = zipWith compareFiles directories directories'
+    forM_ newFiles $ \(x,y) -> do
+        if length y == 0
+            then return ()
+            else do
+                liftIO $ putStr $ (path x) ++ " "
+                liftIO $ print y
+
+    put $ directories'
+
+    liftIO $ threadDelay 1000000
+
+    loop
+
+compareFiles :: Directory -> Directory -> (Directory, [String])
+compareFiles x y = (y, Set.toList $ Set.difference newFiles oldFiles)
+    where oldFiles = Set.fromList $ contents x
+          newFiles = Set.fromList $ contents y
+
+checkDirectories :: DirectoryS ()
+checkDirectories = do
+    directories <- get
+    directories' <- forM directories $ \directory -> do
+        let p = path directory
+            c = contents directory
+        files <- liftIO $ getDirectoryContents p
+        let currentFiles = filter (isSuffixOf ".html") files
+        return $ Directory p currentFiles
+    put directories'
+    return ()
