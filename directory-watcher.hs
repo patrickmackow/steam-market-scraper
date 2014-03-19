48ed9ef6a2ae52e8b080392df42f89c7e1957467
Patrick <patrick@tsar-bomba>
2014-03-18 23:44:49 -0400

    Added threading. TODO: Change hard coded max threads/

--- directory-watcher.hs
+++ directory-watcher.hs
@@ -1,5 +1,7 @@
 import Control.Concurrent
+import Control.Concurrent.SSem
 import Control.Monad
+import Control.Monad.Loops
 import Control.Monad.IO.Class
 import Control.Monad.Trans.State
 import Data.Either.Utils
@@ -45,21 +47,35 @@ loop = do
     directories' <- liftIO $ execStateT checkDirectories $ directories
 
     let newFiles = zipWith compareFiles directories directories'
+
+    put $ directories'
+
+    sem <- liftIO $ new 10
+
     forM_ newFiles $ \(x,y) -> do
         if length y == 0
             then return ()
             else do
-                liftIO $ mapM_
-                    (\file -> processFile (fileType x) (path x) file) y
+                liftIO $ mapM_ forkOS $ map
+                    (\file -> processFile sem (fileType x) (path x) file) y
 
-    put $ directories'
+                liftIO $ whileM_ (liftM not $ checkSemEmpty sem 10) $
+                    threadDelay 1000000
 
     liftIO $ threadDelay 1000000
 
     loop
 
-processFile :: String -> FilePath -> FilePath -> IO ()
-processFile fileType dir path = do
+checkSemEmpty :: SSem -> Int -> IO Bool
+checkSemEmpty sem n = do
+    freeThreads <- getValue sem
+    if freeThreads == n
+        then return True
+        else return False
+
+processFile :: SSem -> String -> FilePath -> FilePath -> IO ()
+processFile sem fileType dir path = do
+    wait sem
     let filePath = dir ++ path
         command = "./dist/build/file-scraper/file-scraper " ++ fileType
             ++ " " ++ filePath
@@ -70,6 +86,7 @@ processFile fileType dir path = do
         ExitFailure n -> do
             renameFile filePath (dir ++ "failed/" ++ path)
             print $ "ERROR: " ++ (show n) ++ " " ++ path
+    signal sem
 
 compareFiles :: Directory -> Directory -> (Directory, [String])
 compareFiles x y = (y, Set.toList $ Set.difference newFiles oldFiles)
